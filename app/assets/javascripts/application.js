6a54a006631c5e91ec7d69621e0265f4db0475ec
Sherwin Yu <sherwin.x.yu@gmail.com>
2012-12-03 04:35:30 -0500

    adding ember-1.0.0 pre 2 and handlebars 1.0beta6 and update applicationjs to use them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,14 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require handlebars
+// require handlebars
 
-//= require ember
+//= require vendor/handlebars-1.0.0.beta.6
+//= require vendor/ember-1.0.0-pre.2
+// require ember
 // require ember-data
 //= require vendor/ember-data-edge
 
 //= require vendor/date
 //= require vendor/moment
+
 
 //= require_self
 //= require sysys
