sherwinyu/sp

commit 5145a8137c13b766c8cf3a7bef463fa3869fc72d

rudimentary keyboard shortcut support for moving to next node

app/assets/javascripts/views/content_field.js.coffee
@@ -33,5 +33,8 @@ Sysys.ContentField = Ember.TextArea.extend
       @get('controller').commitChanges()
       e.preventDefault()
 
+    @$().bind 'keyup', 'down', (e) =>
+      @get('controller').nextNode()
+
   willDestroyElement: ->
     @$().trigger 'remove.autogrow'

app/assets/javascripts/views/humon_node_view.js.coffee
@@ -21,12 +21,17 @@ Sysys.HumonNodeView = Ember.View.extend
     @get('content')?.set 'nodeView', null
 
   willInsertElement: ->
+    @initHotkeys()
     @get('content')?.set 'nodeView', @
 
 
   didInsertElement: ->
 
   initHotkeys: ->
+    @$().bind 'keyup', 'up', (e) =>
+      console.log 'zup'
+
+
     
   commit: ->
     val = @$('.content-field').first().val()

commit 527bdd7ac445f4f102ed78de931e6d46a140111e

fixed bug introduced in 5145a81 -- would call HNV.willInsertElement to bind the node view, but this would cause re-rendering of new elements ala commitChanges with [1, 2, 3]; solution was to move from willInsertElement to didInsertElement; switch content field shift enter from calling dc.commitChanges to dc.commitAndContinue

app/assets/javascripts/views/content_field.js.coffee
@@ -30,7 +30,7 @@ Sysys.ContentField = Ember.TextArea.extend
       e.preventDefault()
 
     @$().bind 'keydown', 'shift+return', (e) =>
-      @get('controller').commitChanges()
+      @get('controller').commitAndContinue()
       e.preventDefault()
 
     @$().bind 'keyup', 'down', (e) =>

app/assets/javascripts/views/detail_controller.js.coffee
@@ -10,7 +10,6 @@ Sysys.DetailController = Ember.Object.extend
     next = @get('activeHumonNode').nextNode()
     @commitChanges()
     last = next.prevNode()
-    debugger
     @set('activeHumonNode', last)
 
   # precondition: activeNode is a literal
@@ -46,8 +45,8 @@ Sysys.DetailController = Ember.Object.extend
     if newNode 
       @set('activeHumonNode', newNode)
 
+      ###
   insertNewElement: ->
-    debugger
     currentNode = @get('currentNode')
     parent = currentNodeget('nodeParent')
     len = parent.get('nodeVal.length')
@@ -58,6 +57,7 @@ Sysys.DetailController = Ember.Object.extend
       else if parent.get('isList')
         empty
     parent.replaceAt(len, 0, obj)
+    ###
 
   init: ->
     stateMgr = Ember.StateManager.create

app/assets/javascripts/views/humon_node_view.js.coffee
@@ -21,11 +21,10 @@ Sysys.HumonNodeView = Ember.View.extend
     @get('content')?.set 'nodeView', null
 
   willInsertElement: ->
-    @initHotkeys()
-    @get('content')?.set 'nodeView', @
-
 
   didInsertElement: ->
+    @initHotkeys()
+    @get('content')?.set 'nodeView', @
 
   initHotkeys: ->
     @$().bind 'keyup', 'up', (e) =>