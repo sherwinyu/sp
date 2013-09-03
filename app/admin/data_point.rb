1f3cd4cc0657cfb1a5df11eb210f741f14d2a6ab
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-09-02 23:50:11 -0500

    add DataPoint activeadmin registration; add timestamps to data point model

---
+++ app/admin/data_point.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register DataPoint do
+  index do
+    column :submitted_at
+    column :created_at
+    column :details
+    default_actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :submitted_at
+      f.input :details
+    end
+    f.actions
+  end
+end
