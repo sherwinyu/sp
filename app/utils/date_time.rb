1439f51b090665c2e9d399d58f18c1a6bef6e7c9
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-10-28 01:36:29 -0400

    add Util.time_to_experienced_dae

--- app/utils/date_time.rb
+++ app/utils/date_time.rb
@@ -17,4 +17,23 @@ class Util
     tz.local_to_utc(time)
   end
 
+  #
+  # time is a Time.
+  # Returns a Date representing the "experienced day (in eastern time)" that time
+  # coresponds to.
+  #
+  # E.g., 3am Tuesday in Eastern Time corresponds to Monday
+  # Current cut off is 03:59:59 counts as previous day, 04:00:00 counts as current day
+  def self.time_to_experienced_date time
+    eastern_tz = TZInfo::Timezone.get('US/Eastern')
+    eastern_time = time.in_time_zone(eastern_tz)
+    experienced_date = eastern_time.to_date
+
+    if eastern_time < experienced_date + 4.hours
+      experienced_date = experienced_date.yesterday if eastern_time
+    end
+    experienced_date
+
+  end
+
 end
--- spec/utils/date_time_spec.rb
+++ spec/utils/date_time_spec.rb
@@ -17,3 +17,41 @@ describe "Util: date time" do
   end
 end
 
+describe "Util: time_to_experienced_date" do
+  let (:sunday) { Date.new(2013, 10, 27) }
+  let (:monday) { Date.new(2013, 10, 28) }
+  let (:tuesday) { Date.new(2013, 10, 29) }
+  let (:eastern_tz) { TZInfo::Timezone.get('US/Eastern') }
+
+  let (:sunday_dawn)    { eastern_tz.local_to_utc(sunday.to_time) + 3.hours + 59.minutes + 59.seconds }
+  let (:sunday_morning) { eastern_tz.local_to_utc(sunday.to_time) + 9.hours }
+  let (:sunday_evening) { eastern_tz.local_to_utc(sunday.to_time) + 18.hours }
+
+  let (:monday_dawn)    { eastern_tz.local_to_utc(monday.to_time) + 3.hours + 59.minutes + 59.seconds }
+  let (:monday_morning) { eastern_tz.local_to_utc(monday.to_time) + 9.hours }
+  let (:monday_evening) { eastern_tz.local_to_utc(monday.to_time) + 18.hours }
+
+  let (:tuesday_dawn)    { eastern_tz.local_to_utc(tuesday.to_time) + 3.hours + 59.minutes + 59.seconds }
+  let (:tuesday_morning) { eastern_tz.local_to_utc(tuesday.to_time) + 9.hours }
+  let (:tuesday_evening) { eastern_tz.local_to_utc(tuesday.to_time) + 18.hours }
+
+
+  it "converts 3am to previous day" do
+    binding.pry
+    Util.time_to_experienced_date(monday_dawn).should eq sunday
+    Util.time_to_experienced_date(tuesday_dawn).should eq monday
+  end
+
+  it "converts 9am to current day" do
+    Util.time_to_experienced_date(sunday_morning).should eq sunday
+    Util.time_to_experienced_date(monday_morning).should eq monday
+    Util.time_to_experienced_date(tuesday_morning).should eq tuesday
+  end
+
+  it "converts 9pm to current day" do
+    Util.time_to_experienced_date(sunday_evening).should eq sunday
+    Util.time_to_experienced_date(monday_evening).should eq monday
+    Util.time_to_experienced_date(tuesday_evening).should eq tuesday
+  end
+
+end
