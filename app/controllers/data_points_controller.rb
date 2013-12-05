5f9ddfce461d497a8104fcbff7eb9390aa78a604
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-12-05 04:29:33 -0500

    Add caching for data points

--- app/controllers/data_points_controller.rb
+++ app/controllers/data_points_controller.rb
@@ -4,7 +4,7 @@ class DataPointsController < ApplicationController
   # GET /data_points
   # GET /data_points.json
   def index
-    @data_points = DataPoint.all
+    @data_points = DataPoint.cached_recent
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -5,6 +5,19 @@ class DataPoint
   field :started_at, type: Time
   field :ended_at, type: Time
   field :details
+  scope :recent, desc(:started_at).limit(10)
+
+  after_save :flush_cache
+
+  def flush_cache
+    Rails.cache.delete([self.class.name, "recent"])
+  end
+
+  def self.cached_recent
+    Rails.cache.fetch [name, "recent"] do
+      recent.to_a
+    end
+  end
 
   def self.find args
     if args.to_i.to_s == args
