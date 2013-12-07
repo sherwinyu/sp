1bc22abcb6c8052db4e90c3cf106f2d6000dedec
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-12-06 23:50:25 -0500

    Mini profiler initializer (move mini profile to right); refactor nav into ti's own .emblem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :authorize
   def authorize
-    Rack::MiniProfiler.authorize_request
+    # Rack::MiniProfiler.authorize_request
   end
 
 end
