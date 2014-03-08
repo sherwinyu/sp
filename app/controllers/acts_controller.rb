6c2669c49e4462256d7d6af2a9f541834557728f
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-03-07 20:15:22 -0500

    Fix Acts#update

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -25,17 +25,10 @@ class ActsController < ApplicationController
 
   def update
     @act = Act.find params[:id]
-    detail = params[:act].delete :detail
-    ret = @act.update_attributes params[:act]
-    detail = {val: detail} if detail.class == Array
-    @act.create_detail detail
-    puts ret, @act.inspect
-    if ret
+    if @act.update_attributes params[:act]
       respond_with @act, status: :ok
     else
-      # TODO(syu): make sure that the client can actually process these errors and that the code is correct
-      # respond_with status: 500
-      respond_with @act, status: 422 #render text: @act.to_json, status: 422
+      respond_with @act, status: 422
     end
   end
 
