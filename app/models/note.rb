7631e4dea4ad063136f65a63df8bfa93d0ba14d1
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-07-30 02:18:09 -0700

    Add notes on notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,41 @@
+# Approximately represents one unit of work
+# One pomodoro
+=begin
+  Note
+    NoteItem
+    NoteItem
+    NoteItem
+
+  Adding warg to warg
+    Error(NoteItem)
+      = Body: "Error blarblarblarblarblar"
+      = Tags: {error: 1, ruby: 1}
+
+So while using the editor:
+
+Note(Goal): finish front end for Note
+  NoteItem(objective)
+    = Body: Clean up CSS
+    = Type: "Objective"
+    = Tags: {estimate: 25min}
+  NoteItem(objective)
+    = Body: Clean up CSS
+    = Tags: (objective)
+
+Note(): Cleaning up CSS reference
+  = references NoteItem(objective){body: Clean up CSS}
+  NoteItem(reference
+
+
+
+
+
+
+=end
+SearchIndex on NoteItem.body
+
+
+
 class Note
   include Mongoid::Document
   include Mongoid::Timestamps
