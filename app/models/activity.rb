20ebc2c774aedde5e314647e5a66f922f544d9cd
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-09-12 02:00:36 -0700

    initial version of migration #wip

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,10 @@ class Activity
   include Mongoid::Document
 
   field :name
+  field :names, type: Array
   has_many :rescue_time_dps
+
+  field :category
+  field :productivity
 
 end
--- db/migrate/20140910065738_add_activities.rb
+++ db/migrate/20140910065738_add_activities.rb
@@ -1,6 +1,15 @@
 class AddActivities < Mongoid::Migration
-  def self.up
 
+  def self.cutoff
+    DateTime.new(2014, 9, 1)
+
+  end
+  def self.rtdps
+    RescueTimeDp.where( :time.gte => 1.week.ago )
+  end
+
+  def self.up
+    rtdps.activities
   end
 
   def self.down
