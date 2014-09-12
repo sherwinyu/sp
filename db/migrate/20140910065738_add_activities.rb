d2bbd3e6dfabc0bdc8ee1d6dfa9e0c344d41d760
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-09-12 10:20:28 -0700

    Migration seems to work, I believe

--- db/migrate/20140910065738_add_activities.rb
+++ db/migrate/20140910065738_add_activities.rb
@@ -2,16 +2,28 @@ class AddActivities < Mongoid::Migration
 
   def self.cutoff
     DateTime.new(2014, 9, 1)
-
   end
+
   def self.rtdps
-    RescueTimeDp.where( :time.gte => 1.week.ago )
+    RescueTimeDp.where(:time.gte => cutoff)
   end
 
   def self.up
-    rtdps.activities
+    # get all activities
+    rtdps.each do |rtdp|
+      rtdp.activities.each do |name, activity|
+        a = Activity.where(names: name).first
+        a ||= Activity.new(names: [name],
+                           category: activity.category,
+                           productivity: activity.productivity)
+        a.save
+      end
+
+      rtdps
+    end
   end
 
   def self.down
   end
+
 end
