ef6c9830656e8d0f76104a6936ab339f274418db
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-09-23 10:13:39 -0700

    #WIP Reworking add_activities migartion#up to use rtrs

--- db/migrate/20140910065738_add_activities.rb
+++ db/migrate/20140910065738_add_activities.rb
@@ -9,18 +9,24 @@ class AddActivities < Mongoid::Migration
   end
 
   def self.up
-    # get all activities
+
     puts "rtdps.length: #{rtdps.length}"
     rtdps.each do |rtdp|
-      acts = []
-      rtdp.activities.each do |name, activity|
-        a = Activity.where(names: name).first
-        a ||= Activity.new(names: [name],
-                           category: activity.category,
-                           productivity: activity.productivity)
-        a.save
-        acts << {a: a.id, duration: activity.duration }
-      end
+
+      activities_list = RescueTimeImporter.activities_list_from_rtrs rtdp.rtrs
+
+      rtdp.acts = activities_list
+      rtdp.save
+
+      # acts = []
+      # rtdp.activities.each do |name, activity|
+      # a = Activity.where(names: name).first
+      # a ||= Activity.new(names: [name],
+                           # category: activity.category,
+                           # productivity: activity.productivity)
+      # a.save
+      # acts << {a: a.id, duration: activity.duration }
+      # end
       rtdp.acts = acts
       rtdp.save
     end
