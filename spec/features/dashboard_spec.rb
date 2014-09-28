e7f88166e76f6a85ea0c1b80354efc8bc71ac40f
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-09-28 04:08:21 -0700

    Remove binding.pry from dashboard spec

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -36,7 +36,6 @@ feature "Dashboard", feature: "dashboard" do
               end
       datetime + delta
     end
-    binding.pry
 
     awake_at_dt = normalize Time.parse("8:30").to_datetime
     up_at_dt = normalize Time.parse("8:35").to_datetime
