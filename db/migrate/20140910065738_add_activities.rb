73147d7d1ba0c8193709253299860e0f14ca1545
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-09-28 04:04:11 -0700

    Removing rtdps cutoff in migration

--- db/migrate/20140910065738_add_activities.rb
+++ db/migrate/20140910065738_add_activities.rb
@@ -1,15 +1,11 @@
 class AddActivities < Mongoid::Migration
 
-  def self.cutoff
-    DateTime.new(2014, 9, 1)
-  end
 
   def self.rtdps
-    RescueTimeDp.where(:time.gte => cutoff, :acts.exists => false)
+    RescueTimeDp.all
   end
 
   def self.up
-
     puts "rtdps.length: #{rtdps.length}"
     rtdps.each do |rtdp|
       rtdp.sync_against_raw
