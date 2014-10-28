be2d79f99c83928897e781603f15e5f476d45129
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-10-28 00:00:45 -0700

    Reorg

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,19 +3,19 @@ class ActivitiesController < ApplicationController
   before_filter :prepare_props
   respond_to :json
 
+  def prepare_props
+    @react_props = Activity.first.as_j
+    @react_props = {
+        activities: ActiveModel::ArraySerializer.new(Activity.recent).as_json
+    }
+  end
+
   def index
     @activities = Activity.recent
     respond_to do |format|
       format.html { render layout: 'with_react_js', template: 'pages/home' }
       format.json { render json: @activities }
     end
-  end
-
-  def prepare_props
-    @react_props = Activity.first.as_j
-    @react_props = {
-        activities: ActiveModel::ArraySerializer.new(Activity.recent).as_json
-    }
   end
 
   def show
