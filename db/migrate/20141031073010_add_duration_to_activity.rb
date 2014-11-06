f5c236965a5563a2e1735d6e61e072d4cb5e71bb
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-11-05 23:39:18 -0800

    WIP

--- db/migrate/20141031073010_add_duration_to_activity.rb
+++ db/migrate/20141031073010_add_duration_to_activity.rb
@@ -1,5 +1,22 @@
 class AddDurationToActivity < Mongoid::Migration
   def self.up
+
+    pairs = Activity.all.map do |activity|
+      [activity.id, 0]
+    end
+
+    activities = Hash[pairs]
+    RescueTimeDp.limit(100).each do |rtdp|
+      rtdp.acts.each do |act|
+        id = act['a']
+        duration = act['duration']
+        activities[id] += duration
+      end
+    end
+  end
+
+  def method_1
+
   end
 
   def self.down
